e418743b5890441ea9f659485457aae0e55b40e9
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 14:08:56 +0200

    Add IFailureHandler interface

--- src/AssertNet.Core/Assertion.cs
+++ src/AssertNet.Core/Assertion.cs
@@ -6,9 +6,26 @@
     public abstract class Assertion
     {
         /// <summary>
+        /// The failure handler of the assertion.
+        /// </summary>
+        private readonly IFailureHandler _failureHandler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Assertion"/> class.
+        /// </summary>
+        /// <param name="failureHandler">The failure handler of the assertion.</param>
+        public Assertion(IFailureHandler failureHandler)
+        {
+            _failureHandler = failureHandler;
+        }
+
+        /// <summary>
         /// Fails an assertion with a specific message.
         /// </summary>
         /// <param name="message">The message to fail with.</param>
-        protected abstract void Fail(string message);
+        protected void Fail(string message)
+        {
+            _failureHandler.Fail(message);
+        }
     }
 }
---
+++ src/AssertNet.Core/IFailureHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace AssertNet.Core
+{
+    /// <summary>
+    /// Interface for failure handlers.
+    /// </summary>
+    public interface IFailureHandler
+    {
+        /// <summary>
+        /// Creates an assertion failure with a certain message.
+        /// </summary>
+        /// <param name="message">The message of the assertion failure.</param>
+        void Fail(string message);
+    }
+}
