67848c7fd11a0cbc2dfaf38529553975d4912e97
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 13:08:31 +0200

    Add abstract Assertion class

---
+++ src/AssertNet.Core/Assertion.cs
@@ -0,0 +1,14 @@
+﻿namespace AssertNet.Core
+{
+    /// <summary>
+    /// Abstract class representing assertions.
+    /// </summary>
+    public abstract class Assertion
+    {
+        /// <summary>
+        /// Fails an assertion with a specific message.
+        /// </summary>
+        /// <param name="message">The message to fail with.</param>
+        protected abstract void Fail(string message);
+    }
+}
