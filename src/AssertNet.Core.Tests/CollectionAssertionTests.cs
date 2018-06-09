0a3ca2893ff63e6436a75e9c557e400356238cd7
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 15:06:00 +0200

    Add CollectionAssertion class

---
+++ src/AssertNet.Core.Tests/CollectionAssertionTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+
+namespace AssertNet.Core.Tests
+{
+    /// <summary>
+    /// Test class for the <see cref="CollectionAssertion"/> class.
+    /// </summary>
+    public class CollectionAssertionTests : ObjectAssertionTests<CollectionAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollectionAssertionTests"/> class.
+        /// </summary>
+        public CollectionAssertionTests()
+        {
+            FailureHandler = new Mock<IFailureHandler>();
+            Assertion = new CollectionAssertion(FailureHandler.Object, new int[] { 1, 2, 3 });
+        }
+    }
+}
---
+++ src/AssertNet.Core/CollectionAssertion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace AssertNet.Core
+{
+    /// <summary>
+    /// Class representing assertions made on collection objects.
+    /// </summary>
+    /// <seealso cref="ObjectAssertion{CollectionAssertion}" />
+    public class CollectionAssertion : ObjectAssertion<CollectionAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollectionAssertion"/> class.
+        /// </summary>
+        /// <param name="failureHandler">The failure handler of the assertion.</param>
+        /// <param name="target">The object which is under test.</param>
+        public CollectionAssertion(IFailureHandler failureHandler, IEnumerable target)
+            : base(failureHandler, target)
+        {
+        }
+    }
+}
