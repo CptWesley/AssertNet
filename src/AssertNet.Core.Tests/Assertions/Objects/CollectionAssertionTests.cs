d0c7df742666dd8d6044faa0f647b6f60f1f3368
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 19:04:50 +0200

    Add size assertions to the collections assertions

--- src/AssertNet.Core.Tests/Assertions/Objects/CollectionAssertionTests.cs
+++ src/AssertNet.Core.Tests/Assertions/Objects/CollectionAssertionTests.cs
@@ -1,4 +1,5 @@
-﻿using AssertNet.Core.Assertions.Objects;
+﻿using System;
+using AssertNet.Core.Assertions.Objects;
 using AssertNet.Core.FailureHandlers;
 using Moq;
 using Xunit;
@@ -62,6 +63,106 @@ namespace AssertNet.Core.Tests.Assertions.Objects
         public void DoesNotContainFailTest()
         {
             CollectionAssertion.DoesNotContain(4, 5, 1, 6);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.AtLeastOnce());
+        }
+
+        /// <summary>
+        /// Checks that the assertion does not fail if the object is correctly empty.
+        /// </summary>
+        [Fact]
+        public void IsEmptyPassTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, Array.Empty<int>()).IsEmpty();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the object is incorrectly not empty.
+        /// </summary>
+        [Fact]
+        public void IsEmptyFailTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, new int[] { 1 }).IsEmpty();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.AtLeastOnce());
+        }
+
+        /// <summary>
+        /// Checks that the assertion does not fail if the object is correctly not empty.
+        /// </summary>
+        [Fact]
+        public void IsNotEmptyPassTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, new int[] { 1 }).IsNotEmpty();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the object is incorrectly empty.
+        /// </summary>
+        [Fact]
+        public void IsNotEmptyFailTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, Array.Empty<int>()).IsNotEmpty();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.AtLeastOnce());
+        }
+
+        /// <summary>
+        /// Checks that the assertion does not fail if the object has the correct size.
+        /// </summary>
+        [Fact]
+        public void HasSizePassTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, new int[] { 1, 2, 3 }).HasSize(3);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the object has the incorrect size.
+        /// </summary>
+        [Fact]
+        public void HasSizeFailTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, Array.Empty<int>()).HasSize(3);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.AtLeastOnce());
+        }
+
+        /// <summary>
+        /// Checks that the assertion does not fail if the object has the minimum size.
+        /// </summary>
+        [Fact]
+        public void HasAtLeastSizePassTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, new int[] { 1, 2, 3 }).HasAtLeastSize(2);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the object does not have the minimum size.
+        /// </summary>
+        [Fact]
+        public void HasAtLeastSizeFailTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, Array.Empty<int>()).HasAtLeastSize(1);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.AtLeastOnce());
+        }
+
+        /// <summary>
+        /// Checks that the assertion does not fail if the object has less than the maximum size.
+        /// </summary>
+        [Fact]
+        public void HasAtMostSizePassTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, new int[] { 5, 2 }).HasAtMostSize(4);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the object has more than the maximum size.
+        /// </summary>
+        [Fact]
+        public void HasAtMostSizeFailTest()
+        {
+            new CollectionAssertion(FailureHandler.Object, new int[] { 1, 2, 3 }).HasAtMostSize(1);
             FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.AtLeastOnce());
         }
     }
--- src/AssertNet.Core/Assertions/Objects/CollectionAssertion.cs
+++ src/AssertNet.Core/Assertions/Objects/CollectionAssertion.cs
@@ -31,6 +31,82 @@ namespace AssertNet.Core.Assertions.Objects
         public IEnumerable<object> Collection { get; }
 
         /// <summary>
+        /// Checks if the enumerable is empty.
+        /// </summary>
+        /// <returns>The current assertion.</returns>
+        public CollectionAssertion IsEmpty()
+        {
+            if (Collection.Any())
+            {
+                Fail($"Expected '[{string.Join(", ", Collection)}]' to be empty.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Checks if the enumerable is not empty.
+        /// </summary>
+        /// <returns>The current assertion.</returns>
+        public CollectionAssertion IsNotEmpty()
+        {
+            if (!Collection.Any())
+            {
+                Fail($"Expected '[{string.Join(", ", Collection)}]' to contain at least 1 element.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Checks if the enumerable has a certain size.
+        /// </summary>
+        /// <param name="size">The size the enumerable should have.</param>
+        /// <returns>The current assertion.</returns>
+        public CollectionAssertion HasSize(int size)
+        {
+            int realSize = Collection.Count();
+            if (realSize != size)
+            {
+                Fail($"Expected '[{string.Join(", ", Collection)}]' to have a length of '{size}', while it was '{realSize}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Checks if the enumerable has at least a certain size.
+        /// </summary>
+        /// <param name="size">The size the enumerable should have.</param>
+        /// <returns>The current assertion.</returns>
+        public CollectionAssertion HasAtLeastSize(int size)
+        {
+            int realSize = Collection.Count();
+            if (realSize < size)
+            {
+                Fail($"Expected '[{string.Join(", ", Collection)}]' to have at least a length of '{size}', while it was '{realSize}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Checks if the enumerable has at most a certain size.
+        /// </summary>
+        /// <param name="size">The size the enumerable should have.</param>
+        /// <returns>The current assertion.</returns>
+        public CollectionAssertion HasAtMostSize(int size)
+        {
+            int realSize = Collection.Count();
+            if (realSize > size)
+            {
+                Fail($"Expected '[{string.Join(", ", Collection)}]' to have at most a length of '{size}', while it was '{realSize}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
         /// Checks if the enumerable contains the values.
         /// </summary>
         /// <param name="values">The values to check for.</param>
