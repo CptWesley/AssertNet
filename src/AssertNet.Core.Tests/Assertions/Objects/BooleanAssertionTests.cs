aa1571ef2049ff065c7b1a2b368237b8586b0d8c
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 16:40:03 +0200

    Add BooleanAssertion class

---
+++ src/AssertNet.Core.Tests/Assertions/Objects/BooleanAssertionTests.cs
@@ -0,0 +1,63 @@
+﻿using AssertNet.Core.Assertions.Objects;
+using AssertNet.Core.FailureHandlers;
+using Moq;
+using Xunit;
+
+namespace AssertNet.Core.Tests.Assertions.Objects
+{
+    /// <summary>
+    /// Test class for the <see cref="BooleanAssertion"/> class.
+    /// </summary>
+    /// <seealso cref="ObjectAssertionTests{BooleanAssertion}" />
+    public class BooleanAssertionTests : ObjectAssertionTests<BooleanAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BooleanAssertionTests"/> class.
+        /// </summary>
+        public BooleanAssertionTests()
+        {
+            FailureHandler = new Mock<IFailureHandler>();
+            Assertion = new BooleanAssertion(FailureHandler.Object, true);
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is correctly true.
+        /// </summary>
+        [Fact]
+        public void IsTruePassTest()
+        {
+            new BooleanAssertion(FailureHandler.Object, true).IsTrue();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is incorrectly false.
+        /// </summary>
+        [Fact]
+        public void IsTrueFailTest()
+        {
+            new BooleanAssertion(FailureHandler.Object, false).IsTrue();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is correctly false.
+        /// </summary>
+        [Fact]
+        public void IsFalsePassTest()
+        {
+            new BooleanAssertion(FailureHandler.Object, false).IsFalse();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is incorrectly true.
+        /// </summary>
+        [Fact]
+        public void IsFalseFailTest()
+        {
+            new BooleanAssertion(FailureHandler.Object, true).IsFalse();
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+    }
+}
---
+++ src/AssertNet.Core/Assertions/Objects/BooleanAssertion.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using AssertNet.Core.FailureHandlers;
+
+namespace AssertNet.Core.Assertions.Objects
+{
+    /// <summary>
+    /// Class representing assertions made on boolean items.
+    /// </summary>
+    /// <seealso cref="ObjectAssertion{BooleanAssertion}" />
+    public class BooleanAssertion : ObjectAssertion<BooleanAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BooleanAssertion"/> class.
+        /// </summary>
+        /// <param name="failureHandler">The failure handler of the assertion.</param>
+        /// <param name="target">The object which is under test.</param>
+        public BooleanAssertion(IFailureHandler failureHandler, bool target)
+            : base(failureHandler, target)
+        {
+            Value = target;
+        }
+
+        /// <summary>
+        /// Gets the boolean value under test.
+        /// </summary>
+        /// <value>
+        /// The boolean value under test.
+        /// </value>
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1623:Property summary documentation must match accessors", Justification = "Does not indicate the state of the object.")]
+        public bool Value { get; }
+
+        /// <summary>
+        /// Asserts that the boolean value is true.
+        /// </summary>
+        /// <returns>The current assertion.</returns>
+        public BooleanAssertion IsTrue()
+        {
+            if (Value == false)
+            {
+                Fail($"Expected 'true', but was 'false'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Asserts that the boolean value is false.
+        /// </summary>
+        /// <returns>The current assertion.</returns>
+        public BooleanAssertion IsFalse()
+        {
+            if (Value == true)
+            {
+                Fail($"Expected 'false', but was 'true'.");
+            }
+
+            return this;
+        }
+    }
+}
