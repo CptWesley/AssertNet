f04dac6b746d054497c5106241eed3a240750413
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 15:19:11 +0200

    Add VoidAssertion class

---
+++ src/AssertNet.Core.Tests/VoidAssertionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Moq;
+using Xunit;
+
+namespace AssertNet.Core.Tests
+{
+    /// <summary>
+    /// Test class for the <see cref="VoidAssertion"/> class.
+    /// </summary>
+    public static class VoidAssertionTests
+    {
+        /// <summary>
+        /// Checks that the constructor functions properly.
+        /// </summary>
+        [Fact]
+        public static void ConstructorTest()
+        {
+            IFailureHandler handler = new Mock<IFailureHandler>().Object;
+            Action action = new Mock<Action>().Object;
+            VoidAssertion assertion = new VoidAssertion(handler, action);
+            Assert.Same(handler, assertion.FailureHandler);
+        }
+    }
+}
---
+++ src/AssertNet.Core/VoidAssertion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AssertNet.Core
+{
+    /// <summary>
+    /// Class representing assertions made on actions.
+    /// </summary>
+    /// <seealso cref="Assertion" />
+    public class VoidAssertion : Assertion
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VoidAssertion"/> class.
+        /// </summary>
+        /// <param name="failureHandler">The failure handler of the assertion.</param>
+        /// <param name="action">The action under test.</param>
+        public VoidAssertion(IFailureHandler failureHandler, Action action)
+            : base(failureHandler)
+        {
+            Action = action;
+        }
+
+        /// <summary>
+        /// Gets the action under test.
+        /// </summary>
+        /// <value>
+        /// The action under test.
+        /// </value>
+        public Action Action { get; }
+    }
+}
