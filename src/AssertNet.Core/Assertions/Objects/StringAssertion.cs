af66f68b1a7171c7ccce995d43f98a8e72bba4ab
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 20:58:28 +0200

    Add string contain assertions

--- src/AssertNet.Core/Assertions/Objects/StringAssertion.cs
+++ src/AssertNet.Core/Assertions/Objects/StringAssertion.cs
@@ -25,5 +25,65 @@ namespace AssertNet.Core.Assertions.Objects
         /// The string under test.
         /// </value>
         public string Value => (string)Target;
+
+        /// <summary>
+        /// Asserts if a string contains a certain substring.
+        /// </summary>
+        /// <param name="substring">Substring which needs to be contained.</param>
+        /// <returns>The current assertion.</returns>
+        public StringAssertion Contains(string substring)
+        {
+            if (!Value.Contains(substring))
+            {
+                Fail($"Expected '{Value}' to contain '{substring}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Asserts if a string does not contain a certain substring.
+        /// </summary>
+        /// <param name="substring">Substring may not be contained.</param>
+        /// <returns>The current assertion.</returns>
+        public StringAssertion DoesNotContain(string substring)
+        {
+            if (Value.Contains(substring))
+            {
+                Fail($"Expected '{Value}' to not contain '{substring}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Asserts if a string contains a certain substring.
+        /// </summary>
+        /// <param name="substring">Substring which needs to be contained.</param>
+        /// <returns>The current assertion.</returns>
+        public StringAssertion ContainsIgnoringCase(string substring)
+        {
+            if (!Value.ToUpperInvariant().Contains(substring.ToUpperInvariant()))
+            {
+                Fail($"Expected '{Value}' to contain '{substring}' while ignoring case.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Asserts if a string does not contain a certain substring.
+        /// </summary>
+        /// <param name="substring">Substring may not be contained.</param>
+        /// <returns>The current assertion.</returns>
+        public StringAssertion DoesNotContainIgnoringCase(string substring)
+        {
+            if (Value.ToUpperInvariant().Contains(substring.ToUpperInvariant()))
+            {
+                Fail($"Expected '{Value}' to not contain '{substring}' while ignoring case.");
+            }
+
+            return this;
+        }
     }
 }
