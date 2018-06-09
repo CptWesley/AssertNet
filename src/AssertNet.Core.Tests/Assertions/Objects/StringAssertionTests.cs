8e7830185bd190790fb3b7ffc1c386099b85fe00
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 20:39:11 +0200

    Add StringAssertion class

---
+++ src/AssertNet.Core.Tests/Assertions/Objects/StringAssertionTests.cs
@@ -0,0 +1,23 @@
+﻿using AssertNet.Core.Assertions.Objects;
+using AssertNet.Core.FailureHandlers;
+using Moq;
+using Xunit;
+
+namespace AssertNet.Core.Tests.Assertions.Objects
+{
+    /// <summary>
+    /// Test class for the <see cref="StringAssertion"/> class.
+    /// </summary>
+    /// <seealso cref="ObjectAssertionTests{StringAssertion}" />
+    public class StringAssertionTests : ObjectAssertionTests<StringAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringAssertionTests"/> class.
+        /// </summary>
+        public StringAssertionTests()
+        {
+            FailureHandler = new Mock<IFailureHandler>();
+            Assertion = new StringAssertion(FailureHandler.Object, "threhterj");
+        }
+    }
+}
---
+++ src/AssertNet.Core/Assertions/Objects/StringAssertion.cs
@@ -0,0 +1,29 @@
+﻿using AssertNet.Core.FailureHandlers;
+
+namespace AssertNet.Core.Assertions.Objects
+{
+    /// <summary>
+    /// Class representing assertions made about strings.
+    /// </summary>
+    /// <seealso cref="ObjectAssertion{StringAssertion}" />
+    public class StringAssertion : ObjectAssertion<StringAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StringAssertion"/> class.
+        /// </summary>
+        /// <param name="failureHandler">The failure handler.</param>
+        /// <param name="target">The target.</param>
+        public StringAssertion(IFailureHandler failureHandler, string target)
+            : base(failureHandler, target)
+        {
+        }
+
+        /// <summary>
+        /// Gets the string under test.
+        /// </summary>
+        /// <value>
+        /// The string under test.
+        /// </value>
+        public string Value => (string)Target;
+    }
+}
