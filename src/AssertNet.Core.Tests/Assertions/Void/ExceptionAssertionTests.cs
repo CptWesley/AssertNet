054561398b3c909141cf9979c8eb229f58860f7e
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-09 16:04:00 +0200

    Add ExceptionAssertion class

---
+++ src/AssertNet.Core.Tests/Assertions/Void/ExceptionAssertionTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AssertNet.Core.Assertions.Void;
+using AssertNet.Core.FailureHandlers;
+using Moq;
+using Xunit;
+
+namespace AssertNet.Core.Tests.Assertions.Void
+{
+    /// <summary>
+    /// Test class for the <see cref="ExceptionAssertion"/> class.
+    /// </summary>
+    public class ExceptionAssertionTests
+    {
+        private readonly Mock<IFailureHandler> _handler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionAssertionTests"/> class.
+        /// </summary>
+        public ExceptionAssertionTests()
+        {
+            _handler = new Mock<IFailureHandler>();
+        }
+
+        /// <summary>
+        /// Checks that the constructor functions properly.
+        /// </summary>
+        [Fact]
+        public void ConstructorTest()
+        {
+            Exception e = new Mock<Exception>().Object;
+            ExceptionAssertion assertion = new ExceptionAssertion(_handler.Object, e);
+            Assert.Same(_handler.Object, assertion.FailureHandler);
+            Assert.Same(e, assertion.Exception);
+        }
+    }
+}
---
+++ src/AssertNet.Core/Assertions/Void/ExceptionAssertion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AssertNet.Core.FailureHandlers;
+
+namespace AssertNet.Core.Assertions.Void
+{
+    /// <summary>
+    /// Class representing assertions made on thrown exceptions.
+    /// </summary>
+    /// <seealso cref="Assertion" />
+    public class ExceptionAssertion : Assertion
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionAssertion"/> class.
+        /// </summary>
+        /// <param name="failureHandler">The failure handler.</param>
+        /// <param name="exception">The exception.</param>
+        public ExceptionAssertion(IFailureHandler failureHandler, Exception exception)
+            : base(failureHandler)
+        {
+            Exception = exception;
+        }
+
+        /// <summary>
+        /// Gets the exception under test.
+        /// </summary>
+        /// <value>
+        /// The exception under test.
+        /// </value>
+        public Exception Exception { get; }
+    }
+}
