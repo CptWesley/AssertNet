b0f45cb650aef7d5baae2d23e86cbf3cc26b1b85
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-10 14:05:28 +0200

    Add missing tests

--- src/AssertNet.Core.Tests/Assertions/Objects/StringAssertionTests.cs
+++ src/AssertNet.Core.Tests/Assertions/Objects/StringAssertionTests.cs
@@ -19,5 +19,87 @@ namespace AssertNet.Core.Tests.Assertions.Objects
             FailureHandler = new Mock<IFailureHandler>();
             Assertion = new StringAssertion(FailureHandler.Object, "threhterj");
         }
+
+        private StringAssertion StringAssertion => (StringAssertion)Assertion;
+
+        /// <summary>
+        /// Checks that the assertion passes if the value contains a substring.
+        /// </summary>
+        [Fact]
+        public void ContainsPassTest()
+        {
+            StringAssertion.Contains("t");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value does not contain a substring.
+        /// </summary>
+        [Fact]
+        public void ContainsFailTest()
+        {
+            StringAssertion.Contains("T");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value does not contain a substring.
+        /// </summary>
+        [Fact]
+        public void DoesNotContainPassTest()
+        {
+            StringAssertion.DoesNotContain("T");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value contains a substring.
+        /// </summary>
+        [Fact]
+        public void DoesNotContainFailTest()
+        {
+            StringAssertion.DoesNotContain("t");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value contains a substring while ignoring cases.
+        /// </summary>
+        [Fact]
+        public void ContainsIgnoringCasePassTest()
+        {
+            StringAssertion.ContainsIgnoringCase("T");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value does not contain a substring while ignoring cases.
+        /// </summary>
+        [Fact]
+        public void ContainsIgnoringCaseFailTest()
+        {
+            StringAssertion.ContainsIgnoringCase("5ft5fre453f34");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value does not contain a substring while ignoring cases.
+        /// </summary>
+        [Fact]
+        public void DoesNotContainIgnoringCasePassTest()
+        {
+            StringAssertion.DoesNotContainIgnoringCase("f457h905f435nh9mfht5");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value contains a substring while ignoring cases.
+        /// </summary>
+        [Fact]
+        public void DoesNotContainIgnoringCaseFailTest()
+        {
+            StringAssertion.DoesNotContainIgnoringCase("T");
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
     }
 }
