da5cffaf8b6dad80f51110e81413d4b34ecf9357
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-10 20:55:55 +0200

    Add NUnit project

---
+++ src/AssertNet.NUnit/NUnitFailureHandler.cs
@@ -0,0 +1,21 @@
+﻿using AssertNet.Core.FailureHandlers;
+using NUnit.Framework;
+
+namespace AssertNet.NUnit
+{
+    /// <summary>
+    /// Failure handler for NUnit.
+    /// </summary>
+    /// <seealso cref="IFailureHandler" />
+    public class NUnitFailureHandler : IFailureHandler
+    {
+        /// <summary>
+        /// Creates an assertion failure with a certain message.
+        /// </summary>
+        /// <param name="message">The message of the assertion failure.</param>
+        public void Fail(string message)
+        {
+            throw new AssertionException(message);
+        }
+    }
+}
---
+++ src/AssertNet.Nunit.Tests/AssertionsTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using AssertNet.Core.Assertions;
+using AssertNet.Core.Assertions.Objects;
+using AssertNet.Core.Assertions.Void;
+using AssertNet.NUnit;
+using NUnit.Framework;
+using static AssertNet.NUnit.Assertions;
+
+namespace AssertNet.Nunit.Tests
+{
+    /// <summary>
+    /// Test class for the <see cref="Assertions"/> class.
+    /// </summary>
+    [TestFixture]
+    public static class AssertionsTests
+    {
+        /// <summary>
+        /// Checks that void assertions are created correctly.
+        /// </summary>
+        [Test]
+        public static void VoidAssertionTest()
+        {
+            Assertion assertion = AssertThat(() => { });
+            Assert.NotNull(assertion);
+            Assert.IsInstanceOf<VoidAssertion>(assertion);
+        }
+
+        /// <summary>
+        /// Checks that exception assertions are created correctly.
+        /// </summary>
+        [Test]
+        public static void ExceptionAssertionTest()
+        {
+            Assertion assertion = AssertThat(new Exception());
+            Assert.NotNull(assertion);
+            Assert.IsInstanceOf<ExceptionAssertion>(assertion);
+        }
+
+        /// <summary>
+        /// Checks that boolean assertions are created correctly.
+        /// </summary>
+        [Test]
+        public static void BooleanAssertionTest()
+        {
+            Assertion assertion = AssertThat(true);
+            Assert.NotNull(assertion);
+            Assert.IsInstanceOf<BooleanAssertion>(assertion);
+        }
+
+        /// <summary>
+        /// Checks that double assertions are created correctly.
+        /// </summary>
+        [Test]
+        public static void DoubleAssertionTest()
+        {
+            Assertion assertion = AssertThat(4.5);
+            Assert.NotNull(assertion);
+            Assert.IsInstanceOf<DoubleAssertion>(assertion);
+        }
+
+        /// <summary>
+        /// Checks that string assertions are created correctly.
+        /// </summary>
+        [Test]
+        public static void StringAssertionTest()
+        {
+            Assertion assertion = AssertThat(string.Empty);
+            Assert.NotNull(assertion);
+            Assert.IsInstanceOf<StringAssertion>(assertion);
+        }
+
+        /// <summary>
+        /// Checks that collection assertions are created correctly.
+        /// </summary>
+        [Test]
+        public static void CollectionAssertionTest()
+        {
+            Assertion assertion = AssertThat(Array.Empty<int>());
+            Assert.NotNull(assertion);
+            Assert.IsInstanceOf<CollectionAssertion>(assertion);
+        }
+
+        /// <summary>
+        /// Checks that single object assertions are created correctly.
+        /// </summary>
+        [Test]
+        public static void SingleAssertionTest()
+        {
+            Assertion assertion = AssertThat(new object());
+            Assert.NotNull(assertion);
+            Assert.IsInstanceOf<SingleAssertion>(assertion);
+        }
+    }
+}
