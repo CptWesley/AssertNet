6c4001f62f7db4a55c4f56f17d77c46442d47ed1
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-10 14:50:53 +0200

    Add comparison assertions

---
+++ src/AssertNet.Core.Tests/Assertions/Objects/DoubleAssertionTests.cs
@@ -0,0 +1,103 @@
+﻿using AssertNet.Core.Assertions.Objects;
+using AssertNet.Core.FailureHandlers;
+using Moq;
+using Xunit;
+
+namespace AssertNet.Core.Tests.Assertions.Objects
+{
+    /// <summary>
+    /// Test class for the <see cref="DoubleAssertion"/> class.
+    /// </summary>
+    /// <seealso cref="ObjectAssertionTests{DoubleAssertion}" />
+    public class DoubleAssertionTests : ObjectAssertionTests<DoubleAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoubleAssertionTests"/> class.
+        /// </summary>
+        public DoubleAssertionTests()
+        {
+            FailureHandler = new Mock<IFailureHandler>();
+            Assertion = new DoubleAssertion(FailureHandler.Object, 500);
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is greater than a certain value.
+        /// </summary>
+        [Fact]
+        public void IsGreaterThanPassTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 20).IsGreaterThan(10);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value is not greater than a certain value.
+        /// </summary>
+        [Fact]
+        public void IsGreaterThanFailTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 15).IsGreaterThan(15);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is greater than or equal to a certain value.
+        /// </summary>
+        [Fact]
+        public void IsGreaterThanOrEqualPassTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 32).IsGreaterThanOrEqual(32);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value is not greater than or equal to a certain value.
+        /// </summary>
+        [Fact]
+        public void IsGreaterThanOrEqualFailTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 2).IsGreaterThanOrEqual(23);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is lesser than a certain value.
+        /// </summary>
+        [Fact]
+        public void IsLesserThanPassTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 12).IsLesserThan(13);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value is not lesser than a certain value.
+        /// </summary>
+        [Fact]
+        public void IsLesserThanFailTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 25).IsLesserThan(10);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+
+        /// <summary>
+        /// Checks that the assertion passes if the value is lesser than or equal to a certain value.
+        /// </summary>
+        [Fact]
+        public void IsLesserThanOrEqualPassTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 2).IsLesserThanOrEqual(2);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Never());
+        }
+
+        /// <summary>
+        /// Checks that the assertion fails if the value is not lesser than or equal to a certain value.
+        /// </summary>
+        [Fact]
+        public void IsLesserThanOrEqualFailTest()
+        {
+            new DoubleAssertion(FailureHandler.Object, 2).IsLesserThanOrEqual(0);
+            FailureHandler.Verify(x => x.Fail(It.IsAny<string>()), Times.Once());
+        }
+    }
+}
---
+++ src/AssertNet.Core/Assertions/Objects/DoubleAssertion.cs
@@ -0,0 +1,89 @@
+﻿using AssertNet.Core.FailureHandlers;
+
+namespace AssertNet.Core.Assertions.Objects
+{
+    /// <summary>
+    /// Class representing assertions made about doubles (and other numeric values).
+    /// </summary>
+    /// <seealso cref="ObjectAssertion{DoubleAssertion}" />
+    public class DoubleAssertion : ObjectAssertion<DoubleAssertion>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoubleAssertion"/> class.
+        /// </summary>
+        /// <param name="failureHandler">The failure handler of the assertion.</param>
+        /// <param name="target">The object which is under test.</param>
+        public DoubleAssertion(IFailureHandler failureHandler, double target)
+            : base(failureHandler, target)
+        {
+        }
+
+        /// <summary>
+        /// Gets the double under test.
+        /// </summary>
+        /// <value>
+        /// The double under test.
+        /// </value>
+        public double Value => (double)Target;
+
+        /// <summary>
+        /// Asserts if a double is greater than a certain value.
+        /// </summary>
+        /// <param name="other">Value which the double should be greater than.</param>
+        /// <returns>The current assertion.</returns>
+        public DoubleAssertion IsGreaterThan(double other)
+        {
+            if (Value <= other)
+            {
+                Fail($"Expected '{Value}' to be greater than '{other}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Asserts if a double is greater than or equal to a certain value.
+        /// </summary>
+        /// <param name="other">Value which the double should be greater than or equal to.</param>
+        /// <returns>The current assertion.</returns>
+        public DoubleAssertion IsGreaterThanOrEqual(double other)
+        {
+            if (Value < other)
+            {
+                Fail($"Expected '{Value}' to be greater than or equal to '{other}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Asserts if a double is lesser than a certain value.
+        /// </summary>
+        /// <param name="other">Value which the double should be lesser than.</param>
+        /// <returns>The current assertion.</returns>
+        public DoubleAssertion IsLesserThan(double other)
+        {
+            if (Value >= other)
+            {
+                Fail($"Expected '{Value}' to be lesser than '{other}'.");
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Asserts if a double is lesser than or equal to a certain value.
+        /// </summary>
+        /// <param name="other">Value which the double should be lesser than or equal to.</param>
+        /// <returns>The current assertion.</returns>
+        public DoubleAssertion IsLesserThanOrEqual(double other)
+        {
+            if (Value > other)
+            {
+                Fail($"Expected '{Value}' to be lesser than or equal to '{other}'.");
+            }
+
+            return this;
+        }
+    }
+}
