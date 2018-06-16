84ab1fe45474b2edb0358ce284e6246233a14236
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-16 21:57:59 +0200

    Add invocation count assertions

--- src/AssertNet.Moq.Tests/Mocks/InvocationAssertionTests.cs
+++ src/AssertNet.Moq.Tests/Mocks/InvocationAssertionTests.cs
@@ -42,5 +42,130 @@ namespace AssertNet.Moq.Tests.Mocks
         {
             Assert.Same(_expression, _assertion.Expression);
         }
+
+        /// <summary>
+        /// Checks that we pass when the invocation has never occured.
+        /// </summary>
+        [Fact]
+        public void NeverPassTest()
+        {
+            _assertion.Never();
+        }
+
+        /// <summary>
+        /// Checks that we fail when the invocation has occured.
+        /// </summary>
+        [Fact]
+        public void NeverFailTest()
+        {
+            _target.Object.GetInt();
+            Assert.Throws<MockException>(() => _assertion.Never());
+        }
+
+        /// <summary>
+        /// Checks that we pass when the invocation has occured.
+        /// </summary>
+        [Fact]
+        public void OncePassTest()
+        {
+            _target.Object.GetInt();
+            _assertion.Once();
+        }
+
+        /// <summary>
+        /// Checks that we fail when the invocation has never occured.
+        /// </summary>
+        [Fact]
+        public void OnceFailTest()
+        {
+            Assert.Throws<MockException>(() => _assertion.Once());
+            _target.Object.GetInt();
+            _target.Object.GetInt();
+            Assert.Throws<MockException>(() => _assertion.Once());
+        }
+
+        /// <summary>
+        /// Checks that we pass when at least 1 invocation has occured.
+        /// </summary>
+        [Fact]
+        public void AtLeastOnceTest()
+        {
+            Assert.Throws<MockException>(() => _assertion.AtLeastOnce());
+            _target.Object.GetInt();
+            _assertion.AtLeastOnce();
+            _target.Object.GetInt();
+            _assertion.AtLeastOnce();
+        }
+
+        /// <summary>
+        /// Checks that we pass when at most 1 invocation has occured.
+        /// </summary>
+        [Fact]
+        public void AtMostOnce()
+        {
+            _assertion.AtMostOnce();
+            _target.Object.GetInt();
+            _assertion.AtMostOnce();
+            _target.Object.GetInt();
+            Assert.Throws<MockException>(() => _assertion.AtMostOnce());
+        }
+
+        /// <summary>
+        /// Checks that we pass when at least 1 invocation has occured.
+        /// </summary>
+        [Fact]
+        public void AtLeastTest()
+        {
+            Assert.Throws<MockException>(() => _assertion.AtLeast(1));
+            _target.Object.GetInt();
+            _assertion.AtLeast(1);
+            _target.Object.GetInt();
+            _assertion.AtLeast(1);
+        }
+
+        /// <summary>
+        /// Checks that we pass when at most 2 invocations have occured.
+        /// </summary>
+        [Fact]
+        public void AtMostTest()
+        {
+            _assertion.AtMost(2);
+            _target.Object.GetInt();
+            _assertion.AtMost(2);
+            _target.Object.GetInt();
+            _assertion.AtMost(2);
+            _target.Object.GetInt();
+            Assert.Throws<MockException>(() => _assertion.AtMost(2));
+        }
+
+        /// <summary>
+        /// Checks that we pass when exactly 2 invocation have occured.
+        /// </summary>
+        [Fact]
+        public void ExactlyTest()
+        {
+            Assert.Throws<MockException>(() => _assertion.Exactly(2));
+            _target.Object.GetInt();
+            Assert.Throws<MockException>(() => _assertion.Exactly(2));
+            _target.Object.GetInt();
+            _assertion.Exactly(2);
+            _target.Object.GetInt();
+            Assert.Throws<MockException>(() => _assertion.Exactly(2));
+        }
+
+        /// <summary>
+        /// Checks that we pass when between 1 and 2 invocation have occured.
+        /// </summary>
+        [Fact]
+        public void BetweenTest()
+        {
+            Assert.Throws<MockException>(() => _assertion.Between(1, 2));
+            _target.Object.GetInt();
+            _assertion.Between(1, 2);
+            _target.Object.GetInt();
+            _assertion.Between(1, 2);
+            _target.Object.GetInt();
+            Assert.Throws<MockException>(() => _assertion.Between(1, 2));
+        }
     }
 }
--- src/AssertNet.Moq/Mocks/InvocationAssertion.cs
+++ src/AssertNet.Moq/Mocks/InvocationAssertion.cs
@@ -37,5 +37,50 @@ namespace AssertNet.Moq.Mocks
         /// The expression of the invocation.
         /// </value>
         public Expression<Action<T>> Expression { get; }
+
+        /// <summary>
+        /// Asserts that the expression was never invoked.
+        /// </summary>
+        public void Never() => Target.Verify(Expression, Times.Never);
+
+        /// <summary>
+        /// Asserts that the expression was invoked once.
+        /// </summary>
+        public void Once() => Target.Verify(Expression, Times.Once);
+
+        /// <summary>
+        /// Asserts that the expression was invoked at least once.
+        /// </summary>
+        public void AtLeastOnce() => Target.Verify(Expression, Times.AtLeastOnce);
+
+        /// <summary>
+        /// Asserts that the expression was invoked at most once.
+        /// </summary>
+        public void AtMostOnce() => Target.Verify(Expression, Times.AtMostOnce);
+
+        /// <summary>
+        /// Asserts that the expression was invoked at least the given amount of times.
+        /// </summary>
+        /// <param name="count">The minimum amount of invocations.</param>
+        public void AtLeast(int count) => Target.Verify(Expression, Times.AtLeast(count));
+
+        /// <summary>
+        /// Asserts that the expression was invoked at most the given amount of times.
+        /// </summary>
+        /// <param name="count">The maximum amount of invocations.</param>
+        public void AtMost(int count) => Target.Verify(Expression, Times.AtMost(count));
+
+        /// <summary>
+        /// Asserts that the expression was invoked exactly the given amount of times.
+        /// </summary>
+        /// <param name="count">The exact amount of invocations.</param>
+        public void Exactly(int count) => Target.Verify(Expression, Times.Exactly(count));
+
+        /// <summary>
+        /// Asserts that the expression was invoked a number of times in a certain range.
+        /// </summary>
+        /// <param name="minimum">The minimum amount of invocations.</param>
+        /// <param name="maximum">The maximum amount of invocations.</param>
+        public void Between(int minimum, int maximum) => Target.Verify(Expression, Times.Between(minimum, maximum, Range.Inclusive));
     }
 }
