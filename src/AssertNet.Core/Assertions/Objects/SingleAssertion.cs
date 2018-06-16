d492b864c5db4c032a468545086e448aa3b795f9
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-06-16 14:19:28 +0200

    Change SingleAssertion to always contain an object type

--- src/AssertNet.Core/Assertions/Objects/SingleAssertion.cs
+++ src/AssertNet.Core/Assertions/Objects/SingleAssertion.cs
@@ -5,16 +5,15 @@ namespace AssertNet.Core.Assertions.Objects
     /// <summary>
     /// Class representing assertions made on single objects.
     /// </summary>
-    /// <typeparam name="TTarget">Type of the object under test.</typeparam>
     /// <seealso cref="ObjectAssertion{TAssert, TTarget}" />
-    public class SingleAssertion<TTarget> : ObjectAssertion<SingleAssertion<TTarget>, TTarget>
+    public class SingleAssertion : ObjectAssertion<SingleAssertion, object>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="SingleAssertion{TTarget}"/> class.
+        /// Initializes a new instance of the <see cref="SingleAssertion"/> class.
         /// </summary>
         /// <param name="failureHandler">The failure handler of the assertion.</param>
         /// <param name="target">The object which is under test.</param>
-        public SingleAssertion(IFailureHandler failureHandler, TTarget target)
+        public SingleAssertion(IFailureHandler failureHandler, object target)
             : base(failureHandler, target)
         {
         }
