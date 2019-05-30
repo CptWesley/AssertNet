e05dd274d645ac598a9b169081b93649e51aa028
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-05-30 11:58:07 +0200

    Add some tests

--- src/AssertNet/FailureHandlers/AssertionFailedException.cs
+++ src/AssertNet/FailureHandlers/AssertionFailedException.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace AssertNet.FailureHandlers
 {
@@ -32,16 +31,6 @@ namespace AssertNet.FailureHandlers
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
         public AssertionFailedException(string message, Exception innerException)
             : base(message, innerException)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AssertionFailedException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="SerializationInfo"></see> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="StreamingContext"></see> that contains contextual information about the source or destination.</param>
-        protected AssertionFailedException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
         {
         }
     }
