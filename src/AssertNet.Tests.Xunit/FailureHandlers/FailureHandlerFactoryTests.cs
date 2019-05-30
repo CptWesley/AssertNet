1d0a2ef813498717cc798eab17326e927ee86f64
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-05-30 11:23:16 +0200

    Add tests for the factory class

---
+++ src/AssertNet.Tests.Xunit/FailureHandlers/FailureHandlerFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using AssertNet.FailureHandlers;
+using Xunit;
+
+namespace AssertNet.Tests.Xunit.FailureHandlers
+{
+    /// <summary>
+    /// Test class for the <see cref="FailureHandlerFactory"/> class.
+    /// </summary>
+    public static class FailureHandlerFactoryTests
+    {
+        /// <summary>
+        /// Checks that the factory creates the correct type.
+        /// </summary>
+        [Fact]
+        public static void FactoryTest()
+        {
+            Assert.IsType<XunitFailureHandler>(FailureHandlerFactory.Create());
+        }
+    }
+}
