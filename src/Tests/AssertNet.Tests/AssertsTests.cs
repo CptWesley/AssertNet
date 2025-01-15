db42246da3dceb6a74299494acd6115c31cea131
Corniel Nobel <corniel@gmail.com>
2025-01-15 11:03:23 +0100

    WIP

---
+++ src/Tests/AssertNet.Tests/AssertsTests.cs
@@ -0,0 +1,43 @@
+using AssertNet.Core.AssertionTypes;
+using AssertNet.Core.Failures;
+using AssertNet.FailureHandlers;
+using System.Numerics;
+using static AssertNet.Asserter;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AssertNet.Tests;
+
+public class Asserts_That
+{
+    [Fact]
+    public void supports_fluent_syntax()
+    {
+        Asserts.That(3).IsEqualTo(3);
+    }
+
+    [Fact]
+    public void is_extendable()
+    {
+        Asserts.ThatX(6m).IsEven();
+    }
+}
+
+file static class AssertionExtesions
+{
+    public static NumberAssertion<TNumber> ThatX<TNumber>(this Asserter _, TNumber value) where TNumber : struct, INumber<TNumber>
+    {
+        return new(FailureHandlerFactory.Create(), value);
+    }
+}
+
+file sealed class NumberAssertion<TNumber>(IFailureHandler failureHandler, TNumber value) : Assertion(failureHandler) where TNumber : struct, INumber<TNumber>
+{
+    public void IsEven()
+    {
+        var two = TNumber.One + TNumber.One;
+        if (value % two != TNumber.Zero)
+        {
+            Fail($"{value} is odd.");
+        }
+    }
+}
