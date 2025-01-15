3405ac4ce094d01453389307d5277fba1a340ae0
Corniel Nobel <corniel@gmail.com>
2025-01-15 17:51:45 +0100

    Extend documentation

--- src/Libraries/AssertNet.Core/AssertionTypes/NumericAssertions.cs
+++ src/Libraries/AssertNet.Core/AssertionTypes/NumericAssertions.cs
@@ -4,9 +4,21 @@ using System.Numerics;
 
 namespace AssertNet.Core.AssertionTypes;
 
+/// <summary>Assertions on <see cref="INumber{TSelf}" />.</summary>
 public static class NumericAssertions
 {
-    public static void IsFinate<TNumber>(this Assertion<TNumber> assertion, string? message = null) where TNumber : struct, INumber<TNumber>
+    /// <summary>Ensures that the number under test is a finite number.</summary>
+    /// <typeparam name="TNumber">
+    /// The type of the number.
+    /// </typeparam>
+    /// <param name="assertion">
+    /// The assertion.
+    /// </param>
+    /// <param name="message">
+    /// The assertion message.
+    /// </param>
+    [FluentSyntax]
+    public static Assertion<TNumber> IsFinite<TNumber>(this Assertion<TNumber> assertion, string? message = null) where TNumber : struct, INumber<TNumber>
     {
         if (!TNumber.IsFinite(assertion.Subject))
         {
@@ -16,9 +28,22 @@ public static class NumericAssertions
                  .Append("To be finate")
                  .Finish());
         }
+
+        return assertion;
     }
 
-    public static void IsZero<TNumber>(this Assertion<TNumber> assertion, string? message = null) where TNumber : struct, INumber<TNumber>
+    /// <summary>Ensures that the number under test is zero.</summary>
+    /// <typeparam name="TNumber">
+    /// The type of the number.
+    /// </typeparam>
+    /// <param name="assertion">
+    /// The assertion.
+    /// </param>
+    /// <param name="message">
+    /// The assertion message.
+    /// </param>
+    [FluentSyntax]
+    public static Assertion<TNumber> IsZero<TNumber>(this Assertion<TNumber> assertion, string? message = null) where TNumber : struct, INumber<TNumber>
     {
         if (!TNumber.IsZero(assertion.Subject))
         {
@@ -28,6 +53,8 @@ public static class NumericAssertions
                 .Append("To be equal to", TNumber.Zero)
                 .Finish());
         }
+
+        return assertion;
     }
 }
 
