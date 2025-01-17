86bbb81d806cd1a1a511ea818925ac1a84f0909f
Wesley Baartman <cptwesley@jojoweba.nl>
2025-01-17 11:51:32 +0200

    no message

--- src/Libraries/AssertNet/AssertionTypes/Extensions/BooleanAssertions.cs
+++ src/Libraries/AssertNet/AssertionTypes/Extensions/BooleanAssertions.cs
@@ -6,51 +6,7 @@ namespace AssertNet;
 /// Class representing assertions made on boolean items.
 /// </summary>
 /// <seealso cref="Assertion{TAssert, TTarget}" />
-public static class BooleanAssertions
+[GenerateAssertionsFor<bool>]
+public static partial class BooleanAssertions
 {
-    /// <summary>
-    /// Asserts that the boolean value is true.
-    /// </summary>
-    /// <param name="assertion">The initial assertion chain.</param>
-    /// <param name="message">Custom message for the assertion failure.</param>
-    /// <typeparam name="TAssert">The type of assertion.</typeparam>
-    /// <returns>The updated assertion chain.</returns>
-    [Assertion]
-    public static TAssert IsTrue<TAssert>(this TAssert assertion, string? message = null)
-        where TAssert : IAssertion<bool>
-    {
-        if (!assertion.Subject)
-        {
-            assertion.Fail(new FailureBuilder("IsTrue()")
-                .Append(message)
-                .Append("Expecting", assertion.Subject)
-                .Append("To be equal to", true)
-                .Finish());
-        }
-
-        return assertion;
-    }
-
-    /// <summary>
-    /// Asserts that the boolean value is false.
-    /// </summary>
-    /// <param name="assertion">The initial assertion chain.</param>
-    /// <param name="message">Custom message for the assertion failure.</param>
-    /// <typeparam name="TAssert">The type of assertion.</typeparam>
-    /// <returns>The updated assertion chain.</returns>
-    [Assertion]
-    public static TAssert IsFalse<TAssert>(this TAssert assertion, string? message = null)
-        where TAssert : IAssertion<bool>
-    {
-        if (assertion.Subject)
-        {
-            assertion.Fail(new FailureBuilder("IsFalse()")
-                .Append(message)
-                .Append("Expecting", assertion.Subject)
-                .Append("To be equal to", false)
-                .Finish());
-        }
-
-        return assertion;
-    }
 }
